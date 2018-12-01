94652d442081dee7e6298c98368ba501afc3d3c1
Rajat Pagare <rpagare@isystango.com>
2018-12-01 17:43:12 +0530

    feat: initial commit

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TestCocoaPods' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for TestCocoaPods
+  
+  pod 'TestFramework', :podspec => '/Users/stplmacmini14/Desktop/TestFramework/TestFramework/TestFramework.podspec' 
+
+  target 'TestCocoaPodsTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'TestCocoaPodsUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- TestCocoaPods/AppDelegate.swift
+++ TestCocoaPods/AppDelegate.swift
@@ -7,13 +7,12 @@
 //
 
 import UIKit
+import TestFramework
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -27,6 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        TestFramework.logToConsole(msg: "Application inactive")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -35,6 +35,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        TestFramework.logToConsole(msg: "Application active")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
@@ -43,4 +44,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-
